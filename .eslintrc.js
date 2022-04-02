6a0974b3cdd5eea5a7610a2c00cd6c8baad2b6b5
Roman1510 <romanvinnick@gmail.com>
2022-04-02 13:52:19 +0200

    adding readme, adding esling settings and configs

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 0,
+    'comma-dangle': 0,
+    'node/no-callback-literal': 0,
+    'react/react-in-jsx-scope': 'off',
+  },
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # react-starter
+
+## _React starter project_ prebuilt with eslint and prettier. Created with Vite
+
+Just clone the repository to use it.
+
+### Settings applied:
+
+- eslint (with standard style guide)
+- prettier
+- esling-plugin-prettier
+- eslint-config-prettier
+
